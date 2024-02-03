404d57f66d214e19baa6e4121455d7992057368d
Sara Mentr <saramentr@gmail.com>
2024-02-03 17:42:12 +0300

    Update models.py

--- mainApp/models.py
+++ mainApp/models.py
@@ -2,13 +2,12 @@ from django.db import models
 import uuid
 
 class UserDetails(models.Model):
-    id = models.UUIDField(
+    result = models.CharField(max_length=50)
+    result_id = models.UUIDField(
         primary_key = True, 
         default = uuid.uuid4,
         editable = False, 
         unique=True
         )
-    name = models.CharField(max_length=50)
-    bank_ac_number = models.PositiveIntegerField(default=0)
-    paypal_number = models.PositiveIntegerField(default=0)
-    ifsc_code = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    timeout = models.PositiveIntegerField(default=0)
